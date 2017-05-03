b74265a6a4e40f6a2647bbebbb0451da34baf037
Ruben Kazumov <kazumov@gmail.com>
2017-05-02 21:53:36 -0500

    dynamic toolbar

--- total-report/js/krr/ui/toolbar-view.js
+++ total-report/js/krr/ui/toolbar-view.js
@@ -5,62 +5,23 @@
  * @returns {krr.ui.ToolbarView}
  */
 krr.ui.ToolbarView = function () {
-    var arrows = {unicode: {left: "&#x25c0;", right: "&#x25b6;", class: {left: "slide left", right: "slide right"}}, awesome: {left: "", right: "", class: {left: "slide left fa fa-chevron-left", right: "slide left fa fa-chevron-right"}}},
-            position = "none",
-            scrollWithButtons = true,
-            scrollWithSlider = true,
-            fadeouts = true;
+    var position = "none";
     var tb = document.createElement("div");
-//    var slider = document.createElement("div");
-    var left = document.createElement("div");
-    left.innerHTML = arrows.unicode.left;
-    var right = document.createElement("div");
-    right.innerHTML = arrows.unicode.right;
     var commands = document.createElement("div");// ul
     var mask = document.createElement("div");
     var fadeoutLeft = document.createElement("div");
     var fadeoutRight = document.createElement("div");
 
-
-    var t;
-    for (var i = 0; i < 20; i++) {
-
-        t = document.createElement("div");// li
-        t.setAttribute("class", "command");
-        commands.appendChild(t);
-        t.innerHTML = "some command";
-
-    }
-
-    tb.appendChild(left);
     tb.appendChild(mask);
-//    mask.appendChild(slider);
     mask.appendChild(commands);
-    tb.appendChild(right);
     tb.appendChild(fadeoutLeft);
     tb.appendChild(fadeoutRight);
 
-    left.setAttribute("class", arrows.unicode.class.left);
-    right.setAttribute("class", arrows.unicode.class.right);
     mask.setAttribute("class", "mask mask-with-sliders");
-//    slider.setAttribute("class", "slider");
     commands.setAttribute("class", "commands");
-//    fadeoutLeft.setAttribute("class", "fadeout-left");
-//    fadeoutRight.setAttribute("class", "fadeout-right");
 
 
-//    var sliderLeft = () => {
-//        return(parseInt(window.getComputedStyle(slider).getPropertyValue("left"), 10));
-//    };
-//    var sliderWidth = () => {
-//        return(parseInt(window.getComputedStyle(slider).getPropertyValue("width"), 10));
-//    };
-//    var maskWidth = () => {
-//        return(parseInt(window.getComputedStyle(mask).getPropertyValue("left"), 10));
-//    };
     var checkFadeoutsVisibility = () => {
-//        console.log(mask.scrollLeft, sliderWidth(), maskWidth());
-//        console.log(mask.clientWidth, mask.scrollWidth, mask.scrollLeft);
         if (mask.scrollLeft > 1) {
             fadeoutLeft.setAttribute("class", "fadeout-left visible");
         } else {
@@ -74,20 +35,8 @@ krr.ui.ToolbarView = function () {
         }
 
     };
-//    var lastSliderLeft = slider.scrollLeft;
     mask.addEventListener("scroll", (e) => {
-//        var l = e.target.scrollLeft;
-//        console.log(e);
         checkFadeoutsVisibility();
-
-//        doorCloser
-
-//        if (l > lastSliderLeft) {
-////            console.log("<-");
-//        } else {
-////            console.log("->");
-//        }
-//        lastSliderLeft = l;
     });
 
 
@@ -116,17 +65,31 @@ krr.ui.ToolbarView = function () {
             }
         }});
 
-    this.scrollWithArrows = true;
-    this.scrollWithMouse = true;
-    this.scrollWithFinger = true;
-    this.arrowsFontAwesome = false;
-    this.arrowsUnicode = true;
-    this.fadeouts = true;
-
-
-
     this.position = position;// default position is "none"
-
     fadeoutRight.setAttribute("class", "fadeout-right");
 };
 
+krr.ui.ToolbarView.prototype.addCommand = function (parameters) {
+    if (!parameters) {
+        throw "An argument is undefined";
+    }
+    if (!parameters.hasOwnProperty("type")) {
+        throw "A property 'type' is undefined";
+    }
+    if (["icon", "text"].indexOf(parameters.type) === -1) {
+        throw "A property 'type' have an unexpected value";
+    }
+    if (!parameters.hasOwnProperty("callback") || !(typeof parameters.callback === 'function')){
+        throw "A property 'callback' is undefined or not a function";
+    }
+    var cmd = document.createElement("div");
+    if (parameters.type === "text" && parameters.hasOwnProperty("caption")) {
+        cmd.setAttribute("class", "command command-text");
+        cmd.innerHTML = parameters.caption;
+    } else if (parameters.type === "icon" && parameters.hasOwnProperty("icon")) {
+        cmd.setAttribute("class", "command command-icon");
+        cmd.innerHTML = "<i class=\"fa fa-" + parameters.icon + "\"></i>";
+    }
+    this.container.appendChild(cmd);
+    cmd.addEventListener("click", parameters.callback);
+};
